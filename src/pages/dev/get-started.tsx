37d5c45c838f21427b1519d37935a56a47a2c46e
Nathaniel <nathaniel_ramirez@icloud.com>
2024-05-27 16:20:38 -0700

    test

--- src/pages/dev/get-started.tsx
+++ src/pages/dev/get-started.tsx
@@ -1,17 +1,37 @@
+import { Link } from "react-router-dom";
+
 function GetStarted() {
   return (
     <main className="flex justify-center items-center mt-40">
-      <div>
+      <div className="max-w-[50vw] xl:max-w-[32vw]">
         <h2 className="text-2xl font-urbanist mb-2">Getting Started</h2>
-        <p className="text-[#bbb] text-sm max-w-[50vw]">
-          <span className="font-bold underline text-white">
+        <p className="text-[#bbb] text-sm mb-2 max-w-[50vw]">
+          <span className="font-bold underline underline-offset-2">
             Disabled until advertisements are available.
           </span>
         </p>
+        <section className="border-2 border-dashed rounded border-[#333] inline-flex gap-2 [&>div]:flex [&>div]:flex-col [&>div]:items-center [&>div]:gap-1 [&>div]:h-[124px] [&>div]:w-[108px] [&_h3]:text-base [&_img]:h-14 [&_a]:text-sm [&_a]:mt-2 [&_a:active]:underline">
+          <div className="bg-[#222]">
+            <h3>HTML5</h3>
+            <img src="/assets/html5.png" draggable={false} />
+            <Link to="/dev/sdk-html5">Open &gt;</Link>
+          </div>
+          <div>
+            <h3>Unity</h3>
+            <img src="/assets/unity.png" draggable={false} />
+            <Link to="/dev/sdk-unity">Open &gt;</Link>
+          </div>
+          <div className="bg-[#222]">
+            <h3>Construct3</h3>
+            <img src="/assets/construct-3.png" draggable={false} />
+            <Link to="/dev/sdk-construct">Open &gt;</Link>
+          </div>
+        </section>
+
         <h2 className="text-2xl font-urbanist mt-2" id="requirements">
           Requirements
         </h2>
-        <ul className="text-[#bbb] list-disc text-sm text-left max-w-[50vw]">
+        <ul className="text-[#bbb] list-disc text-sm text-left">
           <li>
             Fork the{" "}
             <a
