ca7c058a6824bc9ad6acfe7b527cbb86cf2a445d
Nathaniel <nathaniel_ramirez@icloud.com>
2024-06-01 14:41:21 -0700

    fix metadata

--- src/metadata.ts
+++ src/metadata.ts
@@ -56,6 +56,7 @@ export const metadata = [
   { path: "dev/", title: "Developer | nate-games" },
   { path: "dev/get-started", title: "Getting Started | nate-games" },
   { path: "dev/sdk-html5", title: "SDK - HTML5 | nate-games" },
+  { path: "dev/sdk-construct", title: "SDK - Construct 3 | nate-games" },
   { path: "dmca", title: "DMCA | nate-games" },
   { path: "games", title: "Games | nate-games" },
   { path: "*", title: "404" },
